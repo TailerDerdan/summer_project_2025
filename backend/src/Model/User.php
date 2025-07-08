31e26085f4d7f1ab4d654420fa5f4d840f2a2eee
Xanton9one <sas.2337145777@gmail.com>
2025-07-08 12:40:20 +0300

    room_class created

--- backend/src/Model/User.php
+++ backend/src/Model/User.php
@@ -1,7 +1,38 @@
 <?php
-class User {
-    private $id;
-    private $name;
-    public function __construct($id, $name) {
+
+declare(strict_types=1);
+
+namespace App\Model\User;
+
+class User
+{
+    private ?int $userId;
+    private string $nickName;
+    private string $password;
+    private string $avatarPath;
+
+    public function __construct(?int $userId, string $nickName, string $password, string $avatarPath)
+    {
+        $this->userId = $userId;
+        $this->nickName = $nickName;
+        $this->password = $password;
+        $this->avatarPath = $avatarPath;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+    public function getNickName(): string
+    {
+        return $this->nickName;
+    }
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function getAvatarPath(): string
+    {
+        return $this->avatarPath;
     }
 }
