248144a3753909e2dc81a2b76b74ce06aeed54e6
Anton Kozlov <105823298+TailerDerdan@users.noreply.github.com>
2025-07-15 15:02:09 +0300

    поправил громкость

--- fronted/soundsScript/sound.js
+++ fronted/soundsScript/sound.js
@@ -8,6 +8,7 @@ export class Sound
         this.sound.setAttribute("controls", "none");
         this.sound.style.display = "none";
         document.body.appendChild(this.sound);
+        this.sound.volume = 0.2;
     }
 
     play()
