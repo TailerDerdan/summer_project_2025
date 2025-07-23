50bcf157fd189641f5d2d30f284caece904ba2e2
MerBasNik <125216886+MerBasNik@users.noreply.github.com>
2025-07-23 21:25:00 +0300

    миграция

--- backend/migrations/Version20250723182411.php
+++ backend/migrations/Version20250723182411.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250723173026 extends AbstractMigration
+final class Version20250723182411 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250723173026 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE room CHANGE hostId host_id INT NOT NULL');
+        $this->addSql('ALTER TABLE room ADD players_count INT NOT NULL, ADD max_players INT NOT NULL, CHANGE hostId host_id INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE room CHANGE host_id hostId INT NOT NULL');
+        $this->addSql('ALTER TABLE room ADD hostId INT NOT NULL, DROP host_id, DROP players_count, DROP max_players');
     }
 }
