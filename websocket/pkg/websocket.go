95ea9a1afad9e172a6609554a3b7c53a9cf4481b
MerBasNik <125216886+MerBasNik@users.noreply.github.com>
2025-07-23 11:41:39 +0300

    fix багов

--- websocket/pkg/websocket.go
+++ websocket/pkg/websocket.go
@@ -228,7 +228,7 @@ func (h *WebSocketHandler) HandleCreateRoom(w http.ResponseWriter, r *http.Reque
 	w.WriteHeader(http.StatusOK)
 	resp := map[string]interface{}{
 		"roomId": request.RoomID,
-		"ws_url": "ws://ws:8080/ws/room_" + request.RoomID,
+		"ws_url": "ws://87.228.90.3/ws/room_" + request.RoomID,
 	}
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		log.Printf("WebSocket error: %v", err)
