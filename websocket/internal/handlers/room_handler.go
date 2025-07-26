888ef7e742faf7473d08728ef33c8400d8ec20ef
MerBasNik <125216886+MerBasNik@users.noreply.github.com>
2025-07-26 20:24:08 +0300

    исправляю ошибки в отправке сообщений на ws

--- websocket/internal/handlers/room_handler.go
+++ websocket/internal/handlers/room_handler.go
@@ -330,7 +330,7 @@ func (rh *RoomHandler) processRoomMessage(conn *websocket.Conn, roomID, userID s
 	//	return err
 	case "start_game":
 		fmt.Println("$-555-$")
-		gameType := msg.Data["gameType"].(string)
+		gameType, _ := msg.Data["gameType"].(string)
 		err := rh.roomService.StartGame(conn, roomID, userID, gameType)
 		return err
 	}
